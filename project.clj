47189f7f808dbab7a57bd7c4500dd2618cf96fd7
ribelo <r.krzywaznia@gmail.com>
2018-11-17 22:05:32 +0100

    I must sleep... it's working... in just three hours...

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject ribelo/wombat "0.1.0-SNAPSHOT"
+(defproject ribelo/wombat "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/ribelo/ribelo.wombat"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.4.474"]
                  [com.taoensso/encore "2.96.0"]
