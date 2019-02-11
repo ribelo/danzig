84c8d3db31953da5db4e28ef573095f60d2fa886
Ribelo <r.krzywaznia@gmail.com>
2019-02-11 19:11:33 +0100

    This commit is a lie

--- project.clj
+++ project.clj
@@ -3,14 +3,6 @@
   :url "https://github.com/ribelo/ribelo.wombat"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src"]
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.474"]
-                 [com.taoensso/encore "2.96.0"]
-                 [net.cgrand/xforms "0.18.2"]
-                 [clj-time "0.11.0"]
-                 [cheshire "5.8.0"]
-                 [ribelo/visby "0.1.0-SNAPSHOT"]]
-  :global-vars {*warn-on-reflection* true}
-  :profiles {:dev {:dependencies [[criterium "0.4.4"]
-                                  [org.apache.commons/commons-math3 "3.6.1"]]}})
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :global-vars {*warn-on-reflection* true})
